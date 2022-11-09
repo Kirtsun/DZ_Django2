4b5879a6a104e66d5e71aec880b522453ca85185
Kirtsun <olegkirtsun@gmail.com>
2022-11-09 16:07:39 +0200

    commands

--- annotate_aggregate/admin.py
+++ annotate_aggregate/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Publisher, Book, Store
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("name", "pages", "price", "rating", "pubdate",)
+    fieldsets = [
+        ('Name', {'fields': ['name']}),
+    ]
+    list_display_links = ('name', 'pages',)
+    list_filter = ['name', 'price']
+    search_fields = ['name']
+    date_hierarchy = 'timestamp'
+    list_per_page = 10
+    save_as = True
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "age",)
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
