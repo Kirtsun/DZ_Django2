42d263a42f39cf068b3d25f39cab289c62fa16d1
Kirtsun <olegkirtsun@gmail.com>
2022-11-10 13:48:31 +0200

    commands

--- annotate_aggregate/admin.py
+++ annotate_aggregate/admin.py
@@ -3,30 +3,61 @@ from django.contrib import admin
 from .models import Author, Publisher, Book, Store
 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ("name", "pages", "price", "rating", "pubdate",)
-    fieldsets = [
-        ('Name', {'fields': ['name']}),
-    ]
-    list_display_links = ('name', 'pages',)
-    list_filter = ['name', 'price']
-    search_fields = ['name']
-    date_hierarchy = 'timestamp'
-    list_per_page = 10
-    save_as = True
+class BookInLineAuthor(admin.TabularInline):
+    model = Book.authors.through
+    extra = 3
 
 
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ("name", "age",)
+class BookInLine(admin.TabularInline):
+    model = Book
+    extra = 3
+
+
+class BookInLineStore(admin.TabularInline):
+    model = Store.books.through
+    extra = 3
 
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    inlines = [BookInLine]
+    list_display_links = ('name', )
+    list_filter = ['name']
+    search_fields = ['name']
+    save_as = True
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("name", "pages", "price", "rating", "pubdate",)
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Info', {'fields': ['pages', 'price', 'rating', 'pubdate']}),
+    ]
+    list_display_links = ('name', 'pages',)
+    list_filter = ['name', 'price']
+    search_fields = ['name']
+    date_hierarchy = 'pubdate'
+    list_per_page = 10
+    save_as = True
+    inlines = [BookInLineStore, BookInLineAuthor]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "age",)
+    list_display_links = ('name', 'age',)
+    list_filter = ['age']
+    search_fields = ['name']
+    save_as = True
+    inlines = [BookInLineAuthor]
 
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    list_display_links = ('name',)
+    list_filter = ['name']
+    search_fields = ['name']
+    save_as = True
