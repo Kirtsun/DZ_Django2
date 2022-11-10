fa707167bd790b63942993071adebdfc7b54db18
Kirtsun <olegkirtsun@gmail.com>
2022-11-10 23:41:54 +0200

    views

--- annotate_aggregate/urls.py
+++ annotate_aggregate/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = "annotate_aggregate"
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('store_book/<int:pk>/', views.store_book, name='store_book'),
+    path('book/<int:pk>/', views.book, name='book'),
+    path('publisher/<int:pk>/', views.publisher, name='publisher')
 
 ]
--- annotate_aggregate/views.py
+++ annotate_aggregate/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Author, Publisher, Book, Store
+
+
+def index(request):
+    store_list = Store.objects.all()
+    return render(request, 'annotate_aggregate/index.html', {'store_list': store_list})
+
+
+def store_book(request, pk):
+    book_store = Store.objects.prefetch_related('books').filter(pk=pk)
+    return render(request, 'annotate_aggregate/store.html', {'book_store': book_store})
+
+
+def book(request, pk):
+    books = Book.objects.select_related('publisher').prefetch_related('authors').filter(pk=pk)
+    return render(request, 'annotate_aggregate/book.html', {'books': books})
+
+
+def publisher(request, pk):
+    publish = Book.objects.select_related('publisher').filter(pk=pk)
+    return render(request, 'annotate_aggregate/publisher.html')
