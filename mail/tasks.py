83a7a9e37990826a8550145f0cfda042b8f7af9a
Kirtsun <olegkirtsun@gmail.com>
2022-11-16 14:58:43 +0200

    task

--- mail/tasks.py
+++ mail/tasks.py
@@ -5,5 +5,4 @@ from django.core.mail import send_mail as mail_
 
 @shared_task()
 def send_mail(subject, text, from_email):
-    mail_(subject, text, from_email, ['dfs@gmail.com'], fail_silently=False)
-
+    mail_(subject, text, from_email, ['Kirtsun@gmail.com'], fail_silently=False)
